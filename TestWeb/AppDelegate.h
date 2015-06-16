3661723e3e6697ce5ff328f2614205d869762c02
heyanzhi <heyanzhi@jiayanmuzhi.local>
2015-06-16 15:56:56 +0800

    Initial Commit

---
+++ TestWeb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestWeb
+//
+//  Created by heyanzhi on 15/6/16.
+//  Copyright (c) 2015年 heyanzhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestWeb/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestWeb
+//
+//  Created by heyanzhi on 15/6/16.
+//  Copyright (c) 2015年 heyanzhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
