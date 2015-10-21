07f0d60c4786cee83e7628bf742c4750a929de69
hecq <jingxu_he@163.com>
2015-10-21 11:29:32 +0800

    add program files

--- TestWeb/ViewController.m
+++ TestWeb/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
+#import <objc/runtime.h>
+@interface ViewController ()<UIWebViewDelegate>
+@property (weak, nonatomic) IBOutlet UIWebView *mainWebView;
 
 @end
 
@@ -17,11 +18,57 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    NSURL *url =[[NSBundle mainBundle]URLForResource:@"html2.html"
+                                       withExtension:nil];
+    NSMutableURLRequest *request =[NSMutableURLRequest
+                                   requestWithURL:url
+                                   cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:3];
+    _mainWebView.dataDetectorTypes = UIDataDetectorTypeLink;
+    [_mainWebView loadRequest:request];
+    _mainWebView.backgroundColor =[UIColor lightGrayColor];
+    _mainWebView.hidden =YES;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+{
+
+}
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView
+{
+     [_mainWebView stringByEvaluatingJavaScriptFromString:@"test();"];
+}
+
+-(void)webViewDidStartLoad:(UIWebView *)webView
+{
+
+}
+
+-(BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+
+    NSString *host =request.URL.host;
+
+    if ([host isEqualToString:@"ios"]) {
+        NSLog(@"%@",host); 
+        NSLog(@"%@",request.URL.lastPathComponent);
+    }
+    return YES;
+}
+
+- (void)reportUser {
+
+
+   //NSString *str =[_mainWebView stringByEvaluatingJavaScriptFromString:@"reportUser(\'HomePage\');"];
+
+    //[_mainWebView stringByEvaluatingJavaScriptFromString:@"alert(document.title);"];
+    //NSString*str =[_mainWebView stringByEvaluatingJavaScriptFromString:@"getIOSAliInfo()"];
 }
 
 @end
